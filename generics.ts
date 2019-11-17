457042d11a85849602e3b6f3a628c1d6ef51de04
gdgkirkley <gdgkirkley@gmail.com>
2019-11-17 13:35:25 -0800

    Generic & generic constraint

---
+++ generics.ts
@@ -0,0 +1,76 @@
+class ArrayOfNumbers {
+  constructor(public collection: number[]) {}
+
+  get(index: number): number {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfStrings {
+  constructor(public collection: string[]) {}
+
+  get(index: number): string {
+    return this.collection[index];
+  }
+}
+
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+new ArrayOfAnything<string>(["a", "b", "c"]);
+new ArrayOfAnything<number>([1, 2, 3]);
+
+// Example of generics with functions
+
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(["a", "b", "c"]);
+// OR
+printAnything([1, 2, 3]); // Type inference knows that this is for numbers
+
+// Generic Constraints
+
+class Cars {
+  print(): void {
+    console.log("Car!");
+  }
+}
+
+class Houses {
+  print(): void {
+    console.log("House!");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars<Printable>([new Houses(), new Cars()]);
